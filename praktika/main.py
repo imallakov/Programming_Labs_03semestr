af2a820e75a7d914c05534ca81089bd83002930b
Dried System <driedsys@student.21-school.ru>
2023-11-17 15:35:41 +0500

    finished

--- praktika/main.py
+++ praktika/main.py
@@ -19,9 +19,24 @@ def second():
     plt.show()
 
 
+def third():
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    h = 0.1
+    x = np.arange(-5, 5, h)
+    y = np.arange(-5, 5, h)
+    a = 2
+    b = 5
+    x, y = np.meshgrid(x, y)
+    z = a * x ** 2 - b * y ** 2
+    ax.plot_surface(x, y, z)
+    plt.show()
+
+
 def main():
     first()
     second()
+    third()
 
 
 if __name__ == "__main__":
