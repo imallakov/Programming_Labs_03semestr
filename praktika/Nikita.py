954b9d4a037159bd4671dd5dc63ca6ff7ce6c3bf
Yakup Allakov <72393805+imallakov@users.noreply.github.com>
2023-12-18 10:56:36 +0500

    Update Nikita.py

--- praktika/Nikita.py
+++ praktika/Nikita.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 
 def first():
     fig, axs = plt.subplots(2)
-    x = np.arange(-2, 2, 0.1)
-    y = x ** 3 - 6 * x ** 2 + 2
+    x = np.arange(0, 1, 0.1)
+    y = x * np.exp(-2*x**2)
     axs[0].plot(x, y)
-    t = np.arange(0, 6 * np.pi, 0.5)
-    a = 2
-    L = 2
-    x = a * (t - L * np.sin(t))
-    y = a * (1 - L * np.cos(t))
+    t = np.arange(0, 2 * np.pi, 0.1)
+    a = 1
+    b = 2.5
+    x = (b - a) * np.cos(t) - a * np.cos((b - a) * t / a)
+    y = (b - a) * np.sin(t) - a * np.sin((b - a) * t / a)
     axs[1].plot(x, y)
     plt.show()
 
@@ -22,10 +22,9 @@ def graph_3d():
     h = 0.1
     x = np.arange(-5, 5, h)
     y = np.arange(-5, 5, h)
-    a = 2
-    b = 5
+    a = 0.1
     x, y = np.meshgrid(x, y)
-    z = a * x ** 2 - b * y ** 2
+    z = np.exp(-a*(x**2+y**2))
     ax.plot_surface(x, y, z)
     plt.show()
 
